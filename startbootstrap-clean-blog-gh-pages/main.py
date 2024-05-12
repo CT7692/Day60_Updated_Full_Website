b1e42473b4cf05182d2a57e1e81de73deec460a0
Joseph Rydel <jrydel92@gmail.com>
2024-05-11 21:07:43 -0500

    Initial commit.

---
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+from security import safe_requests
+
+app = Flask(import_name="main")
+
+def get_json_blogs():
+    json_blogs = safe_requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
+
+    return json_blogs
+
+
+
+
+@app.route('/')
+def home():
+    blogs = get_json_blogs()
+    h1 = "Angela's Blog"
+    subheading = "A Blog Theme by Start Bootstrap"
+    image = "static/assets/img/home-bg.jpg"
+
+    return render_template("index.html", blogs=blogs ,h1=h1, subheading=subheading, image=image)
+
+@app.route('/about')
+def about():
+    h1 = "About Me"
+    subheading = "This is what I do."
+    image = "static/assets/img/about-bg.jpg"
+
+    return render_template("about.html", h1=h1, subheading=subheading, image=image)
+
+@app.route('/contact')
+def contact():
+    h1 = "Contact Me"
+    subheading = "Have questions? I have answers."
+    image = "static/assets/img/contact-bg.jpg"
+
+    return render_template("contact.html", h1=h1, subheading=subheading, image=image)
+@app.route('/post/<int:id>')
+def post(**kwargs):
+    index = int(kwargs['id']) - 1
+    blogs = get_json_blogs()
+    blog = blogs[index]
+    image = blog['image_url']
+    h1 = blog['title']
+    subheading = blog['subtitle']
+    body = blog['body']
+
+    return render_template(
+        "post.html", h1=h1, subheading=subheading, image=image, body=body)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
