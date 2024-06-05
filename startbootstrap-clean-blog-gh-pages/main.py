688e8320b2791aa02a4c88a47d5399df33b29f7e
Joseph Rydel <79807440+CT7692@users.noreply.github.com>
2024-06-05 09:47:57 -0500

    main.py

--- startbootstrap-clean-blog-gh-pages/main.py
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -49,7 +49,7 @@ def contact():
 
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
-            connection.login(user=my_email, password="itksbmzvrgzlxnen")
+            connection.login(user=my_email, password=pw)
             connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=full_email)
 
         return render_template("confirmation.html", api_key=api_token)
